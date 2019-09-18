c3bc917e5cd3a23fadc1b85a365c819e0311cfc8
Sreeja G <sreejagpt@gmail.com>
2019-09-18 11:24:04 +1000

    Test for summary page added

---
+++ src/pages/Summary.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import Summary from './Summary.js';
+
+test('shows a summary text and button', () => {
+    const {queryByText} = render(
+        <Summary/>,
+        )
+
+    const expectedSummaryText = 'Period starts in X days';
+    expect(queryByText(expectedSummaryText)).toBeTruthy();
+
+    const expectedButtonText = 'Started?';
+    expect(queryByText(expectedButtonText)).toBeTruthy();
+    })
