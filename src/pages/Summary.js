565286191f5e10a580354306af85af6fbce29b2c
Sreeja G <sreejagpt@gmail.com>
2019-09-18 10:59:14 +1000

    First commit

---
+++ src/pages/Summary.js
@@ -0,0 +1,13 @@
+import React,{ Fragment } from 'react';
+
+const Summary = () => {
+  return (
+      <Fragment>
+        <h1>Period starts in X days</h1>
+        <button>Started?</button>
+      </Fragment>
+      );
+}
+
+export default Summary;
+
