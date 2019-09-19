33e59cc10ba235b9310d778edbf2e15a9fe893ae
Sreeja G <sreejagpt@gmail.com>
2019-09-19 14:04:01 +1000

    Run prettier on all files

--- src/pages/Summary.test.js
+++ src/pages/Summary.test.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import Summary from './Summary.js';
+import React from 'react'
+import { render } from '@testing-library/react'
+import Summary from './Summary.js'
 
 test('shows a summary text and button', () => {
-    const {queryByText} = render(
-        <Summary/>,
-        )
+    const { queryByText } = render(<Summary />)
 
-    const expectedSummaryText = 'Period starts in X days';
-    expect(queryByText(expectedSummaryText)).toBeTruthy();
+    const expectedSummaryText = 'Period starts in X days'
+    expect(queryByText(expectedSummaryText)).toBeTruthy()
 
-    const expectedButtonText = 'Started?';
-    expect(queryByText(expectedButtonText)).toBeTruthy();
-    })
+    const expectedButtonText = 'Started?'
+    expect(queryByText(expectedButtonText)).toBeTruthy()
+})
