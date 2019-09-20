23c724d202f31694c7242605546811131ac1dd7e
Sreeja G <sreejagpt@gmail.com>
2019-09-20 10:13:42 +1000

    WIP - calling s3 for dates data using correct file name

--- src/utils/cycleCalculator.js
+++ src/utils/cycleCalculator.js
@@ -1,5 +1,8 @@
-import { getLatestDateFileName } from './s3.js'
+import { getObject, getLatestDateFileName } from './s3.js'
 
 export const getPeriodStartsIn = async () => {
-    getLatestDateFileName()
+    const { REACT_APP_CYCLE_DATA_OBJECT } = process.env
+    const cycleData = await getObject(REACT_APP_CYCLE_DATA_OBJECT)
+    const latestDateFileName = await getLatestDateFileName()
+    const latestDateObject = await getObject(latestDateFileName)
 }
--- src/utils/cycleCalculator.test.js
+++ src/utils/cycleCalculator.test.js
@@ -1,4 +1,7 @@
-jest.mock('./s3.js')
+jest.mock('./s3.js', () => ({
+    getLatestDateFileName: jest.fn(() => Promise.resolve('latest-file-name')),
+    getObject: jest.fn(() => Promise.resolve()),
+}))
 import { getObject, getLatestDateFileName } from './s3.js'
 import { getPeriodStartsIn } from './cycleCalculator.js'
 
@@ -6,5 +9,13 @@ describe('Cycle Calculator', () => {
     it('should get the latest file name in correct directory and then fetch its contents', async () => {
         await getPeriodStartsIn()
         expect(getLatestDateFileName).toHaveBeenCalled()
+        expect(getObject).toHaveBeenCalledWith('latest-file-name')
+    })
+
+    it("should request user's cycle data", async () => {
+        const cycleDataFileName = 'cycle-data-file-name'
+        process.env.REACT_APP_CYCLE_DATA_OBJECT = cycleDataFileName
+        await getPeriodStartsIn()
+        expect(getObject).toHaveBeenCalledWith(cycleDataFileName)
     })
 })
