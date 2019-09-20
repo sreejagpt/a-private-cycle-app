55c71048cbffa2bff7a0cbe5aee5a1134a5a77ad
Sreeja G <sreejagpt@gmail.com>
2019-09-20 11:16:53 +1000

    Rewrite test to be able to mock implementation multiple times

--- src/utils/cycleCalculator.test.js
+++ src/utils/cycleCalculator.test.js
@@ -1,11 +1,21 @@
-jest.mock('./s3.js', () => ({
-  getLatestDateFileName: jest.fn(() => Promise.resolve('latest-file-name')),
-  getObject: jest.fn(() => Promise.resolve()),
-}))
-import { getObject, getLatestDateFileName } from './s3.js'
+import { getLatestDateFileName, getObject } from './s3.js'
+jest.mock('./s3.js')
 import { getPeriodStartsIn } from './cycleCalculator.js'
 
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
 describe('Cycle Calculator', () => {
+  let cycleDataFileName = 'cycle-data-file-name'
+  process.env.REACT_APP_CYCLE_DATA_OBJECT = cycleDataFileName
+
+  beforeEach(() => {
+    getLatestDateFileName.mockImplementation(() =>
+      Promise.resolve('latest-file-name')
+    )
+  })
+
   it('should get the latest file name in correct directory and then fetch its contents', async () => {
     await getPeriodStartsIn()
     expect(getLatestDateFileName).toHaveBeenCalled()
@@ -13,9 +23,18 @@ describe('Cycle Calculator', () => {
   })
 
   it("should request user's cycle data", async () => {
-    const cycleDataFileName = 'cycle-data-file-name'
-    process.env.REACT_APP_CYCLE_DATA_OBJECT = cycleDataFileName
     await getPeriodStartsIn()
     expect(getObject).toHaveBeenCalledWith(cycleDataFileName)
+  })
+
+  xit('should calculate next period by adding cycle days to most recent start date and subtracting from today', async () => {
+    getObject.mockImplementation(name => {
+      const contents =
+        name === cycleDataFileName
+          ? { cycleLengthDays: 28 }
+          : { startDate: new Date() }
+      Promise.resolve(contents)
+    })
+    const days = await getPeriodStartsIn()
   })
 })
