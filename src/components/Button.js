650db427ba2ecd0d883d05364ad1577b13e095d3
Sreeja G <sreejagpt@gmail.com>
2019-09-23 15:18:01 +1000

    Button shows different text based on whether on period or no

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,21 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 import styles from './Button.module.css'
 
-const Button = () => {
-  return <button className={styles.button}>Started?</button>
+const Button = ({ onPeriodNow }) => {
+  return (
+    <button className={styles.button}>
+      {onPeriodNow ? 'Ended?' : 'Started?'}
+    </button>
+  )
+}
+
+Button.propTypes = {
+  onPeriodNow: PropTypes.bool,
+}
+
+Button.defaultProps = {
+  onPeriodNow: false,
 }
 
 export default Button
--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Button from './Button.js'
 
 test("shows a button that says 'Started'? by default", () => {
@@ -7,5 +7,21 @@ test("shows a button that says 'Started'? by default", () => {
 
   const expectedButtonText = 'Started?'
   const element = findByText(expectedButtonText)
+  expect(element).toBeTruthy()
+})
+
+test("Button text changes to 'Ended'? when onPeriodNow is true", () => {
+  const { findByText } = render(<Button onPeriodNow={true} />)
+
+  const element = findByText('Ended?')
+
+  expect(element).toBeTruthy()
+})
+
+test("Button text changes to 'Started'? when onPeriodNow is false", () => {
+  const { findByText } = render(<Button onPeriodNow={false} />)
+
+  const element = findByText('Started?')
+
   expect(element).toBeTruthy()
 })
